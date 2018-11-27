2b3293357cf59124124294a0eb3ad56f15e1379d
skarolk <skarolkiewicz@gmail.com>
2018-11-26 20:49:09 -0500

    added react routes and  rough home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="homeContainer">
+      <div className="homeText">
+        <h1>Scary Movie Club is a like a book club but for Horror Movies</h1>
+      </div>
+      <div className="homeButtonContainer">
+        <button className="homeButton">
+          Join a Scary Movie Club now!
+        </button>
+      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './index.css';
 
-import App from './components/App';
+import MovieList from './components/MovieList';
+import Home from './components/Home';
 import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.render((
   <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>,
+    <Router>
+      <React.Fragment>
+        <Route exact path="/" component={Home} />
+        <Route path="/ratings" component={MovieList} />
+      </React.Fragment>
+    </Router>
+  </Provider>),
   document.getElementById('root')
 );
 
