618b6dab9f83581f26a84dd95d830ff052252345
skarolk <skarolkiewicz@gmail.com>
2018-11-30 13:09:05 -0500

    added action cable

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const API_ROOT = 'http://localhost:4000';
+export const API_WS_ROOT = 'ws://localhost:4000/cable';
+export const HEADERS = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
